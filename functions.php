ca33076f1be6f9418645cd34549fe80fee834a9f
Adnan Crnovrsanin <adnancrnovrsanin48@gmail.com>
2024-01-23 23:21:39 +0100

    Adding on the functionality

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ function fancy_lab_scripts(){
 	wp_enqueue_script( 'flexslider-min-js', get_template_directory_uri() . '/inc/flexslider/jquery.flexslider-min.js', array( 'jquery' ), '', true );
 	wp_enqueue_style( 'flexslider-css', get_template_directory_uri() . '/inc/flexslider/flexslider.css', array(), '', 'all' );
 	wp_enqueue_script( 'flexslider-js', get_template_directory_uri() . '/inc/flexslider/flexslider.js', array( 'jquery' ), '', true );
-
+	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -82,7 +82,6 @@ function fancy_lab_config(){
 		add_theme_support( 'wc-product-gallery-lightbox' );
 		add_theme_support( 'wc-product-gallery-slider' );
 
-		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
 
         /**
